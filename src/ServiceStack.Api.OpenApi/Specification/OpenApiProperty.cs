cc7dbdaf6ec71658c6b11cb19b76b5e2340f0ee4
丁诚昊 <dimchumhaul@163.com>
2019-05-09 11:44:18 +0800

    财务

--- src/ServiceStack.Api.OpenApi/Specification/OpenApiProperty.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.Serialization;
-
-namespace ServiceStack.Api.OpenApi.Specification
-{
-    [DataContract]
-    public class OpenApiProperty : OpenApiDataTypeSchema
-    {
-    }
-}
